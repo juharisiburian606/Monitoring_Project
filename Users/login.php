1bf0853d135afc93aa386d79efd5078414180f42
juharisiburian606 <juharisiburian606@gmail.com>
2025-12-01 10:29:31 +0700

    Update login.php

--- Users/login.php
+++ Users/login.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 }
 
                 // ⬇ Finance
-                if ($jabatan === "finance") {
+                if ($jabatan === "Finance") {
                     header("Location: Finance/Dashboard.php");
                     exit;
                 }
